6837a26dba2626c5bc199b0360a9905689903e65
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-10-22 12:51:42 -0400

    update setup files

--- functions_practice.py
+++ functions_practice.py
@@ -1,41 +1,33 @@
-# in terminal, navigate to the file folder
-# type python3
-# type "from functions_practice import hello" to import the function
-# type "hello()" to run the function
+#A function named hello() that prints a greeting to the user. 
+#This function should accept no arguments and returns nothing.
 def hello():
     print("Hello, Earthling!")
 
+#A function named pack() that accepts three arguments, 
+#and returns a single list with the three arguments inside as list elements.
+def pack(a, b, c):
+    return [a, b, c]
 
-
-# in terminal, navigate to the file folder
-# type python3
-# type "from functions_practice import pack" to import the function
-# type "my_pack = pack("pb&j", "kettle chips", "green tea")" to call the function with some sample arguments and store the result in a variable called my_pack
-# Type "print(my_pack)" to print the contents of the my_pack variable.
-def pack(sandwich, chips, drink):
-    print("I am packing a", sandwich, "some", chips, "and a", drink)
-    return[sandwich, chips, drink]
-
-
-
-# in terminal, navigate to the file folder
-# type python3
-# type "from functions_practice import eat_lunch" to import the function
-
-# what's for lunch? define my lunch items and then eat_lunch>>>
-# type "my_lunch = ["sandwich", "chips", "apple"]" and then "eat_lunch(my_lunch)" to call the function with some sample arguments 
-# ~or~
-# type "eat_lunch(["apple", "sandwich", "chips", "drink"])" to call the function with some sample arguments
-
-# "My lunchbox is empty!"
-# call it separately with an empty array "empty_lunch = []", then the lunchbox is empty: "eat_lunch(empty_lunch)""
-# ~or~
-# oh no! an empty lunchbox! type "eat_lunch([])" to skip lunch.
-
-def eat_lunch(lunch_items):
-    if not lunch_items:
-        print("My lunchbox is empty!")
+#A function called eat_lunch(). 
+# This function should accept a list of any length, 
+# and print out a series of strings that say "First I eat __" 
+# (the first element of the list), 
+# and "Next I eat ___" (for the following elements in the list). 
+# If the list is empty, print "My lunchbox is empty!". 
+# The function should not return anything.
+def eat_lunch(lunchbox):
+    if len(lunchbox) == 0:
+        print("Oh no! An empty luncbox!! 😭")
     else:
-        print("First I eat the", lunch_items[0])
-        for item in lunch_items[1:]:
-            print("Next I eat", item)
+        print("First I eat " + lunchbox[0])
+        for i in range(1, len(lunchbox)):
+            print("Next I eat " + lunchbox[i])
+        print("I'm full!")
+
+
+# Call the functions:
+hello()
+print(pack(1, 2, 3))
+print(pack("apple", "banana", "carrot"))
+eat_lunch(["apple", "banana", "carrot", "doughnut"])
+eat_lunch([])
